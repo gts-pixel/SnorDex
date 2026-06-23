28683de10a38af4819b643b7af8ed50d14e60996
カビゴンSnorlax <tsaqifts45@gmail.com>
2026-06-23 12:18:48 +0700

    UPDATE

--- src/main/java/PokemonData.java
+++ src/main/java/PokemonData.java
@@ -1,6 +1,6 @@
 public class PokemonData {
     public int id;
-    public String name, types, gen;
+    public String name, types, spesies, eggGroup, genderRatio, catchRate, baseExp;
     public int total, hp, atk, def, spA, spD, spe;
     public String ability, height, weight, hiddenAbility;
 
